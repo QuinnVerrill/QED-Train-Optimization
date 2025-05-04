8b48cc44891248a0e5b3511a04653d9934998301
Quinn Verrill <80296114+QuinnVerrill@users.noreply.github.com>
2025-05-04 19:59:08 -0400

    debugging our crazy constraint violations
    added cleanedTotalOptimization to get rid of the junk code we're not using for optimization anymore

--- combinedNonlincon.m
+++ combinedNonlincon.m
@@ -5,8 +5,8 @@ function [c, ceq] = combinedNonlincon(x)
     
     % distance constraint setup
     distCon = 35; % distance threshold <-- parameter to tune
-    index = 2; % starting from the second constraint.
-    num_stations = length(x) / 2;
+    index = 1; % starting from the second constraint.
+    num_stations = length(x(1:12)) / 2;
     % for loop to generate pairwise distance constraints between each station
     for i = 1:num_stations
         for j = i+1:num_stations  % To avoid duplicate constraints (e.g., (1,2) and (2,1))
@@ -20,8 +20,8 @@ function [c, ceq] = combinedNonlincon(x)
     %%% Train Optimization constraints   
     cd = predict(cd_mdl,x(13:16));
     v = TrainVelocity(cd);
-    minVel = 70; %paramter to tune!!!!!! (in m/s)
-    minCars = 6; %paramater to tune!!!! assuming 80 passengers per car - 480 capacity 
+    minVel = 60; %paramter to tune!!!!!! (in m/s)
+    minCars = 4; %paramater to tune!!!! assuming 80 passengers per car - 480 capacity 
     %setting maximum velocity & minimum velocity
     c(index+1) = v - x(21); %x(21) is max velocity set by saftey subsystem 
     c(index+2) = minVel - v;
@@ -32,6 +32,15 @@ function [c, ceq] = combinedNonlincon(x)
     % maximum number of accidents per year
     maxAccidents = 3; % paramater to tune!!!!!!!
     c(index+4) = accident_count_function(x(16:21)) - maxAccidents;
+
+    cd_input = x(13:16);
+    lb_cd = [15 15 10 3];
+    ub_cd = [50 50 20 15];
+    c(end+1:end+4) = cd_input - ub_cd;
+    c(end+5:end+8) = lb_cd - cd_input;
+
    
     ceq = [];
 end
+
+
