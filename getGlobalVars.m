015ab786dbad0c63f2ecbd6e29abf3692fc71abd
laurent1171 <lthorbecke11@gmail.com>
2025-05-05 14:34:21 -0400

    removed unused files

--- getGlobalVars.m
+++
@@ -1,7 +0,0 @@
-function [r1,r2,r3] = getGlobalVars
-global V_maximum
-global number_cars
-global cd_mdl
-r1 = V_maximum;
-r2 = number_cars;
-r3 = cd_mdl;
--- setGlobalConstraintVar.m
+++
@@ -1,7 +0,0 @@
-function setGlobalConstraintVar(val1, val2, val3)
-global constraint1
-global constraint2
-global constraint3
-constraint1 = val1;
-constraint2 = val2;
-constraint3 = val3;
